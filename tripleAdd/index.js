0046f58d93e5fb47a095d3c851cca278ad426f56
henry <tarou.88@gmail.com>
2019-12-23 19:47:03 +0900

    🎉initial commit

---
+++ tripleAdd/index.js
@@ -0,0 +1,11 @@
+// curry 
+function tripleAdd(num1) {
+  return function (num2) {
+    return function (num3) {
+      return num1 + num2 + num3
+    }
+  }
+}
+
+ // Return 60
+console.log(tripleAdd(10)(20)(30))
