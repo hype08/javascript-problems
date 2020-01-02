dddfc6f201d2a04e011ce8440abd4356c19babf5
henry <tarou.88@gmail.com>
2020-01-02 11:36:18 +0900

    flatten an Array

--- 014 Array Flattening/index.js
+++ 014 Array Flattening/index.js
@@ -1,5 +1,5 @@
 let arrays = [[1], [2,3,4], [5,6,7], [8,9,10, "string", false]]
 
-let flattened = arrays.reduce([],(a, b) => a.concat(b),[])
+let flattened = arrays.reduce((a, b) => a.concat(b),[])
 
 console.log(flattened)
