4ca2d8a9e2735e90a35e0130ac9fa22f1269b57a
henry <tarou.88@gmail.com>
2020-01-02 18:11:32 +0900

    2019 Array Flattening

--- 014 Array Flattening/index.js
+++ 014 Array Flattening/index.js
@@ -3,3 +3,9 @@ let arrays = [[1], [2,3,4], [5,6,7], [8,9,10, "string", false]]
 let flattened = arrays.reduce((a, b) => a.concat(b),[])
 
 console.log(flattened)
+
+
+// ES2019
+
+let ES2019 = ['Dog', ['Sheep', 'Wolf']].flat()
+console.log(ES2019)
